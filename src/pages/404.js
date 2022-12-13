4e1da92666275d9677918a75fb3738cabb521faf
sec0x07 <84969651+sec0x07@users.noreply.github.com>
2022-12-13 07:11:42 +0700

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,10 +6,10 @@ const NotFound = () => (
   <Layout>
     <section className="flex flex-col items-center justify-center">
       <h1 className="text-3xl md:text-4xl font-medium mb-4">
-        Not Found this URL :(
+        Not Found this URL :( visit my blog
       </h1>
       <Button>
-  <a href="https://parkerzanta-blog.my.id" className="mx-7">My Blog</a>
+  <a href="https://parkerzanta-blog.my.id" className="mx-7">Click here</a>
      </Button>
     </section>
   </Layout>
