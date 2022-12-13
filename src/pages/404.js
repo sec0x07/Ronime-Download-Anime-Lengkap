238ff163a9172a87f3d1f228aaa304eebdab2919
sec0x07 <84969651+sec0x07@users.noreply.github.com>
2022-12-13 07:09:57 +0700

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,7 +8,9 @@ const NotFound = () => (
       <h1 className="text-3xl md:text-4xl font-medium mb-4">
         Not Found this URL :(
       </h1>
-      <Button><a className="mx-7">Back to Home</a></Button><br><Button><a href="https://parkerzanta-blog.my.id" className="mx-7">Blog</a></Button>
+      <Button>
+  <a href="https://parkerzanta-blog.my.id" className="mx-7">My Blog</a>
+     </Button>
     </section>
   </Layout>
 );
