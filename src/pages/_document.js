0a16e9c2c26986dc20c1974bf0919339e18bfa7c
sec0x07 <84969651+sec0x07@users.noreply.github.com>
2023-02-23 05:51:46 +0700

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,136 +12,136 @@ class MyDocument extends Document {
           <meta name="theme-color" content="#fff" />
           <meta
             name="keywords"
-            content="KaraNime, Download Anime anime batch Subtitle Indonesia, website download anime, download batch anime, download anime, download anime terlengkap, website download anime terlengkap"
+            content="KaraNime, Download Anime, anime batch Subtitle Indonesia, website download anime, download batch anime, download anime, download anime terlengkap, website download anime terlengkap"
           />
           <link
             type="image/png"
             sizes="16x16"
             rel="icon"
-            href="https://domain-ko.xyz/karanime.png"
+            href="https://assets.xs2.dev/karanime.png"
           />
           <link
             type="image/png"
             sizes="32x32"
             rel="icon"
-            href="https://domain-ko.xyz/karanime.png"
+            href="https://assets.xs2.dev/karanime.png"
           />
           <link
             type="image/png"
             sizes="96x96"
             rel="icon"
-            href="https://domain-ko.xyz/karanime.png"
+            href="https://assets.xs2.dev/karanime.png"
           />
           <link
             type="image/png"
             sizes="120x120"
             rel="icon"
-            href="https://domain-ko.xyz/karanime.png"
+            href="https://assets.xs2.dev/karanime.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="72x72"
-            href="https://domain-ko.xyz/karanime.png"
+            href="https://assets.xs2.dev/karanime.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="96x96"
-            href="https://domain-ko.xyz/karanime.png"
+            href="https://assets.xs2.dev/karanime.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="144x144"
-            href="https://domain-ko.xyz/karanime.png"
+            href="https://assets.xs2.dev/karanime.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="192x192"
-            href="https://domain-ko.xyz/karanime.png"
+            href="https://assets.xs2.dev/karanime.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="512x512"
-            href="https://domain-ko.xyz/karanime.png"
+            href="https://assets.xs2.dev/karanime.png"
           />
           <link
             rel="apple-touch-icon"
             type="image/png"
             sizes="57x57"
-            href="https://domain-ko.xyz/karanime.png"
+            href="https://assets.xs2.dev/karanime.png"
           />
           <link
             rel="apple-touch-icon"
             type="image/png"
             sizes="60x60"
-            href="https://domain-ko.xyz/karanime.png"
+            href="https://assets.xs2.dev/karanime.png"
           />
           <link
             rel="apple-touch-icon"
             type="image/png"
             sizes="72x72"
-            href="https://domain-ko.xyz/karanime.png"
+            href="https://assets.xs2.dev/karanime.png"
           />
           <link
             rel="apple-touch-icon"
             type="image/png"
             sizes="76x76"
-            href="https://domain-ko.xyz/karanime.png"
+            href="https://assets.xs2.dev/karanime.png"
           />
           <link
             rel="apple-touch-icon"
             type="image/png"
             sizes="114x114"
-            href="https://domain-ko.xyz/karanime.png"
+            href="https://assets.xs2.dev/karanime.png"
           />
           <link
             rel="apple-touch-icon"
             type="image/png"
             sizes="120x120"
-            href="https://domain-ko.xyz/karanime.png"
+            href="https://assets.xs2.dev/karanime.png"
           />
           <link
             rel="apple-touch-icon"
             type="image/png"
             sizes="144x144"
-            href="https://domain-ko.xyz/karanime.png"
+            href="https://assets.xs2.dev/karanime.png"
           />
           <link
             rel="apple-touch-icon"
             type="image/png"
             sizes="152x152"
-            href="https://domain-ko.xyz/karanime.png"
+            href="https://assets.xs2.dev/karanime.png"
           />
           <link
             rel="apple-touch-icon"
             type="image/png"
             sizes="180x180"
-            href="https://domain-ko.xyz/karanime.png"
+            href="https://assets.xs2.dev/karanime.png"
           />
           <link
             color="#26E07F"
             rel="mask-icon"
-            href="https://domain-ko.xyz/karanime.png"
+            href="https://assets.xs2.dev/karanime.png"
           />
           <meta
             name="msapplication-square70x70logo"
-            content="https://domain-ko.xyz/karanime.png"
+            content="https://assets.xs2.dev/karanime.png"
           />
           <meta
             name="msapplication-TileImage"
-            content="https://domain-ko.xyz/karanime.png"
+            content="https://assets.xs2.dev/karanime.png"
           />
           <meta
             name="msapplication-square150x150logo"
-            content="https://domain-ko.xyz/karanime.png"
+            content="https://assets.xs2.dev/karanime.png"
           />
           <meta
             name="msapplication-square310x310logo"
-            content="https://domain-ko.xyz/karanime.png"
+            content="https://assets.xs2.dev/karanime.png"
           />
           <meta name="msapplication-TileColor" content="#C0FFEE"></meta>
         </Head>
