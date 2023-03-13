e97870d189acf6dc853a88e2da8ef6d09fa880cb
sec0x07 <84969651+sec0x07@users.noreply.github.com>
2023-03-14 05:52:09 +0700

    Update index.js

--- src/layout/index.js
+++ src/layout/index.js
@@ -11,16 +11,14 @@ const Layout = ({ children }) => (
         content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
       />
       <meta name="google-site-verification" content="Wb246OBYzaqXtALQ7tVsWze53MWMTrulMHrL8BnvDGA" />
-  
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-WT590PD75V"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-WT590PD75V');
-</script>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-WT590PD75V"></script>
+      <script>
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+ 
+           gtag('config', 'G-WT590PD75V');
+      </script>
 
     </Head>
     <div className="min-h-screen flex flex-col justify-between">
